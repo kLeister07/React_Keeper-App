e0bc259da33e85c0c8d8ef429496dc4c7794ab85
Kevin Leister <kevinleister07@gmail.com>
2022-10-19 13:54:17 -0500

    initial commit

---
+++ src/components/Note.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Note = () => {
+    return (
+        <div className='note'>
+            <h1>Note</h1>
+            <p>stuff</p>
+        </div>
+    );
+};
+
+export default Note;
